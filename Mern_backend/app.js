09d12317d447890a4e15314b614af90e8397e5dc
AlanTruu <127467587+AlanTruu@users.noreply.github.com>
2025-08-06 11:00:16 -0700

    Changed CORS options

--- Mern_backend/app.js
+++ Mern_backend/app.js
@@ -19,7 +19,7 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
-    origin: env_1.APP_ORIGIN,
+    origin: [env_1.APP_ORIGIN, env_1.APP_ORIGIN.replace(/\/$/, ''), env_1.APP_ORIGIN + '/'],
     credentials: true
 }));
 app.use((0, cookie_parser_1.default)());
